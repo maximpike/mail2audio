990c091f862832f4babd509f66e1d6dbf022dfd9
maxpike <maximilianpike@gmail.com>
2026-02-10 12:43:12 +0000

    feat[#28]: scaffold React TypeScript frontend with Vite

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,6 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 import EmailDetail from "./pages/EmailDetail";
@@ -13,6 +15,12 @@ function App() {
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/email/:id" element={<EmailDetail />} />
           </Routes>
+          <ToastContainer
+              position="top-right"
+              autoClose={3000}
+              hideProgressBar={false}
+              closeOnClick
+          />
       </BrowserRouter>
   )
 }
